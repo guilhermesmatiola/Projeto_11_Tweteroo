56f922c3345a4ce3f32b102436ef2628c8a7c9aa
guilhermesmatiola <guimatiola@gmail.com>
2022-06-17 14:37:35 -0300

    posts sendo executados

--- index.js
+++ index.js
@@ -3,23 +3,53 @@ import express from 'express';
 import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
+const user = [
+  {
+    username: 'bobesponja', 
+    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+  }
+];
 
-// 1/1/2022
+const tweets = [
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub"
+  }
+];
 
-app.get('/', (req, res) => {
+ const newestTweets = [
+ 	{
+ 		username: "bobesponja",
+ 		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+ 	  tweet: "eu amo o hub"
+ 	}
+ ];
+
+app.post('/sign-up', (req, res) => {
+
+  const {username, avatar} = req.body;
+  user.push({username:username, avatar:avatar});
+  res.sendStatus(200);
     
-   
+});
+
+app.post('/tweets', (req, res) => {
+  const [username, tweet]=req.body;
+  tweets.push({username:username, tweet:tweet});
+  res.sendStatus(200);
+});
+
+app.get('/tweets', (req, res) => {
 
   
+
+  res.send(newestTweets);
 });
 
-app.get('/', (req, res) => {
-  res.send(holidays);
-});
 
-
-
-app.listen(5002, ()=>{
-    console.log(chalk.bold.yellow("ola o server ta indo"));
+app.listen(5000, ()=>{
+    console.log(chalk.bold.yellow("server on"));
 });
